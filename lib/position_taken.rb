395dbf63a69f1ef3a869721ec0846e0455d23481
Andrew Zajac <ajzajac@icloud.com>
2019-07-30 16:55:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == "" || " " || nil
     return false
-  elsif board[index] != "" || " "
+  elsif board[index] == "x" || "o"
     return true
   else return true
 end
